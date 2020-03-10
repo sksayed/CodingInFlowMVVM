73e45d2f5eed38d55c1e153497074704323e8665
Itsoul Lab 001 <itsoul.lab.001@gmail.com>
2020-03-10 08:43:16 +0600

    Entity note has been created

--- app/src/main/java/lab/itsoul/com/dailygoods/app/archexample/Note.java
+++ app/src/main/java/lab/itsoul/com/dailygoods/app/archexample/Note.java
@@ -6,12 +6,36 @@ import androidx.room.PrimaryKey;
 
 @Entity( tableName = "note_table")
 public class Note {
+
     @PrimaryKey(autoGenerate = true)
     private int id ;
-
+    private int priority ;
     private String title ;
-
     private String description ;
 
-    private int priority ;
+    //since id is auto generated
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    //since all the fields are private ,we need public getter
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
