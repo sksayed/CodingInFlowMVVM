baa99670daf44797217fd1ec04bb3ef52934a50b
Itsoul Lab 001 <itsoul.lab.001@gmail.com>
2020-03-10 10:40:31 +0600

    Database has been improved with a call back

--- app/src/main/java/lab/itsoul/com/dailygoods/app/archexample/NoteDatabase.java
+++ app/src/main/java/lab/itsoul/com/dailygoods/app/archexample/NoteDatabase.java
@@ -1,10 +1,16 @@
 package lab.itsoul.com.dailygoods.app.archexample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.List;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -18,9 +24,35 @@ public abstract class NoteDatabase extends RoomDatabase {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(callback)
                     .build();
         }
-        return instance ;
+        return instance;
+    }
+
+    private static RoomDatabase.Callback callback = new Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbTaskAsync((NoteDatabase) db).execute();
+        }
+    };
+
+    private static class PopulateDbTaskAsync extends AsyncTask<Void, Void, Void> {
+        INoteDao noteDao ;
+
+        PopulateDbTaskAsync(NoteDatabase database){
+            noteDao = database.noteDao() ;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert( new Note("Go to bazr", "buy things for keto diet" , 1));
+            noteDao.insert( new Note("Go to NAamaz", "buy things for keto diet" , 2));
+            noteDao.insert( new Note("Go to gym", "buy things for keto diet" , 3));
+            noteDao.insert( new Note("Go to chittagong", "buy things for keto diet" , 4));
+            return null;
+        }
     }
 
 
